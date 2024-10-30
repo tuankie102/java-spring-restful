e305e50ece3f23fea8d3809d3d3d1f7a268acbe6
Tuan Kiet <Tuankie102@gmail.com>
2024-10-30 14:28:15 +0700

    create api for create and delete a user

--- src/main/java/vn/hoidanit/jobhunter/controller/HelloController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/HelloController.java
@@ -1,13 +1,34 @@
 package vn.hoidanit.jobhunter.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import vn.hoidanit.jobhunter.domain.User;
+import vn.hoidanit.jobhunter.service.UserService;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/")
-    public String getHelloWorld() {
-        return "Hello World (Tuấn Kiệt)";
+    private final UserService userService;
+
+    public HelloController(UserService userService) {
+        this.userService = userService;
     }
+
+    @PostMapping("/user")
+    public User CreateNewUser(@RequestBody User user) {
+        User postmanUser = this.userService.handleCreateUser(user);
+        return postmanUser;
+    }
+
+    @DeleteMapping("/user/{id}")
+    public String DeleteUser(@PathVariable("id") long id) {
+        this.userService.handleDeleteUser(id);
+        return "Delete a user successful!";
+    }
+
 }
