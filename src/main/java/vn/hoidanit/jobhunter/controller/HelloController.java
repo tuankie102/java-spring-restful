c1dbab66eb2158342b2a6ecf84d03a26a5278f36
Tuan Kiet <Tuankie102@gmail.com>
2024-10-31 18:44:56 +0700

    first in chapter exception

--- src/main/java/vn/hoidanit/jobhunter/controller/HelloController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/HelloController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.jobhunter.domain.User;
+import vn.hoidanit.jobhunter.service.IdInvalidException;
 import vn.hoidanit.jobhunter.service.UserService;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -33,8 +35,16 @@ public class HelloController {
         return ResponseEntity.status(HttpStatus.CREATED).body(postmanUser);
     }
 
+    @ExceptionHandler(value = IdInvalidException.class)
+    public ResponseEntity<String> handleIdException(IdInvalidException idInvalidException) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(idInvalidException.getMessage());
+    }
+
     @DeleteMapping("/users/{id}")
-    public ResponseEntity<String> DeleteUser(@PathVariable("id") long id) {
+    public ResponseEntity<String> DeleteUser(@PathVariable("id") long id) throws IdInvalidException {
+        if (id > 1500) {
+            throw new IdInvalidException("Id khong duoc lon hon 1500");
+        }
         this.userService.handleDeleteUser(id);
         return ResponseEntity.ok("Delete a user successful!");
     }
---
+++ src/main/java/vn/hoidanit/jobhunter/service/IdInvalidException.java
@@ -0,0 +1,9 @@
+package vn.hoidanit.jobhunter.service;
+
+public class IdInvalidException extends Exception {
+
+    public IdInvalidException(String message) {
+        super(message);
+    }
+
+}
