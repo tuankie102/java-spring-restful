bd53a322a998b519439447fba7351b946c20789a
Tuan Kiet <Tuankie102@gmail.com>
2024-10-31 23:00:54 +0700

    format responsebody before write

--- src/main/java/vn/hoidanit/jobhunter/service/error/GlobalException.java
+++ src/main/java/vn/hoidanit/jobhunter/service/error/GlobalException.java
@@ -5,12 +5,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import vn.hoidanit.jobhunter.domain.RestResponse;
+
 @RestControllerAdvice
 public class GlobalException {
 
     @ExceptionHandler(value = IdInvalidException.class)
-    public ResponseEntity<String> handleIdException(IdInvalidException idInvalidException) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(idInvalidException.getMessage());
+    public ResponseEntity<RestResponse<Object>> handleIdException(IdInvalidException idInvalidException) {
+
+        RestResponse<Object> res = new RestResponse<Object>();
+        res.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        res.setError(idInvalidException.getMessage());
+        res.setMessage("IdInValidException");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
     }
 
 }
