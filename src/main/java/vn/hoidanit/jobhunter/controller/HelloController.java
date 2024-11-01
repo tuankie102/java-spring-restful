3c4bbf5ee3a4d501180b4e392467060d7c3b691c
Tuan Kiet <Tuankie102@gmail.com>
2024-11-01 14:26:38 +0700

    initialize spring security

--- src/main/java/vn/hoidanit/jobhunter/controller/HelloController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/HelloController.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,18 +16,23 @@ import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.service.error.IdInvalidException;
 import vn.hoidanit.jobhunter.service.UserService;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HelloController {
 
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
-    public HelloController(UserService userService) {
+    public HelloController(UserService userService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/users")
     public ResponseEntity<User> CreateNewUser(@RequestBody User user) {
+        String hashPassword = this.passwordEncoder.encode(user.getPassword());
+        user.setPassword(hashPassword);
         User postmanUser = this.userService.handleCreateUser(user);
         // ResponseEntity.ok(postmanUser);
         return ResponseEntity.status(HttpStatus.CREATED).body(postmanUser);
@@ -58,6 +63,11 @@ public class HelloController {
     public ResponseEntity<User> updateUser(@RequestBody User user) {
 
         return ResponseEntity.ok(this.userService.handleCreateUser(user));
+    }
+
+    @GetMapping("/")
+    public String getMethodName() {
+        return new String("Hello World");
     }
 
 }
